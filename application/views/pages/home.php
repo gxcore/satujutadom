6c59116453de86dd837be5ac2da184b399d2ef01
Ardhy <grantz.x.core@gmail.com>
2017-07-24 16:58:43 +0700

    update content dr email

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -4,5 +4,5 @@
 	<img class="image-block" src="<?php echo base_url('assets'); ?>/images/1jutadomain-logo.png" alt="1 juta domain"><br>
 	<p class="lead">
 		<br>
-		Program 1 Juta Domain .ID adalah lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla magna nulla, molestie vitae finibus nec, placerat quis nisi. Suspendisse consectetur, mi sed maximus varius, massa nibh vulputate mauris, eu dapibus sem nibh eget arcu. Ut ac lectus tellus. Mauris vel magna at metus tempus mollis. Suspendisse rutrum, lectus id ullamcorper tempor, eros lacus consequat elit, tempor fermentum neque purus at quam. Nam nec tortor semper, accumsan dui ut, vehicula purus. Pellentesque ultricies ex vel ligula porta cursus.
+		Program 1 Juta Domain .ID adalah Bantuan Pemerintah kepada masyarakat yang dikhususkan untuk UMKM, Sekolah, Pesantren, Komunitas dan Desa berupa pemberian Domain dan Hosting secara gratis selama 1 (satu) Tahun. Diharapkan dengan adanya Program 1 Juta Domain .ID, Dunia pendidikan dapat mendukung tumbuhnya konten positif di Indonesia, Komunitas dapat mendorong tumbuhnya inovasi kreatif dalam negeri, UMKM yang berpotensial dapat difasilitasi menjadi pelaku bisnis online dan Desa dapat memperkenalkan potensi desa masing-masing ke pihak luar.
 	</p>
--- application/views/pages/unduh.php
+++ application/views/pages/unduh.php
@@ -2,10 +2,8 @@
 	<h1>Unduh Dokumen terkatit</h1>
 	<div>
 		<br>
-		<a class="button-dl btn btn-outline-info btn-lg" href="<?php echo base_url('persyaratan'); ?>"><i class="fa fa-download" aria-hidden="true"></i> PEDOMAN UMUM BANTUAN PEMERINTAH</a>
-		<a class="button-dl btn btn-outline-info btn-lg" href="<?php echo base_url('persyaratan'); ?>"><i class="fa fa-download" aria-hidden="true"></i> PETUNJUK TEKNIS BANTUAN 1 JUTA DOMAIN .ID</a>
-		<a class="button-dl btn btn-outline-info btn-lg" href="<?php echo base_url('persyaratan'); ?>"><i class="fa fa-download" aria-hidden="true"></i> MATERI DOMAIN .ID</a>
-		<a class="button-dl btn btn-outline-info btn-lg" href="<?php echo base_url('persyaratan'); ?>"><i class="fa fa-download" aria-hidden="true"></i> FORMULIR PENDAFTARAN BANTUAN</a>
-		<a class="button-dl btn btn-outline-info btn-lg" href="<?php echo base_url('persyaratan'); ?>"><i class="fa fa-download" aria-hidden="true"></i> TEMPLATE WEBSITE</a>
-		<a class="button-dl btn btn-outline-info btn-lg" href="<?php echo base_url('persyaratan'); ?>"><i class="fa fa-download" aria-hidden="true"></i> PETUNJUK TEMPLATE WEBSITE</a>	
+		<a class="button-dl btn btn-outline-info btn-lg" href="<?php echo base_url('docs/a. pedoman umum.pdf'); ?>" download="1Juta Domain - Pedoman Umum"><i class="fa fa-download" aria-hidden="true"></i> PEDOMAN UMUM BANTUAN PEMERINTAH</a>
+		<a class="button-dl btn btn-outline-info btn-lg" href="<?php echo base_url('docs/b. Petunjuk Teknis.pdf'); ?>" download="1Juta Domain - Petunjuk Teknis"><i class="fa fa-download" aria-hidden="true"></i> PETUNJUK TEKNIS BANTUAN 1 JUTA DOMAIN .ID</a>
+		<a class="button-dl btn btn-outline-info btn-lg" href="<?php echo base_url('docs/c. Materi Umum.pdf'); ?>" download="1Juta Domain - Materi Umum"><i class="fa fa-download" aria-hidden="true"></i> MATERI DOMAIN .ID</a>
+		<a class="button-dl btn btn-outline-info btn-lg" href="<?php echo base_url('docs/d. form calon penerima domain.pdf'); ?>" download="1Juta Domain - Formulir Pendaftaran"><i class="fa fa-download" aria-hidden="true"></i> FORMULIR PENDAFTARAN BANTUAN</a>
 	</div>
